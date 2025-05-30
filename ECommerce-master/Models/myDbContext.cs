5d3713c4cb3281499a27acee8814079e93cb410e
ArfaouiManel <manelarfaoui04@gmail.com>
2025-05-31 00:25:00 +0200

    init

---
+++ ECommerce-master/Models/myDbContext.cs
@@ -0,0 +1,22 @@
+﻿    using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Models
+{
+    public class myDbContext : DbContext
+    {
+        public DbSet<customer> customers { get; set; }
+        public DbSet<admin> admins { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Cart> carts { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<contact> contacts { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ECommerceDB;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
